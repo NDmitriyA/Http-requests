029814d6667ce124566289adde9f42bc627cdbd9
Дмитрий Никифоров <nikiforov_d.1973@mail.ru>
2022-03-31 16:06:14 +0400

    New files

---
+++ main.py
@@ -0,0 +1,11 @@
+import requests
+
+name_url = "https://superheroapi.com/api/2619421814940190/search/"
+superheroes = [{'name': 'Hulk'}, {
+    'name': 'Captain America'}, {'name': 'Thanos'}]
+
+for hero in superheroes:
+    hero_r = requests.get(name_url + hero['name'])
+    hero['intelligence'] = int(
+        hero_r.json()['results'][0]['powerstats']['intelligence'])
+print(sorted(superheroes, key=lambda hero: -hero['intelligence'])[0]['name'])
